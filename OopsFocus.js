46c05d88e66268eb5b851d8f7880be110016f2ac
Anna Petion <annapetion2@gmail.com>
2025-12-16 23:48:03 -0500

    started js yayyyy

---
+++ OopsFocus.js
@@ -0,0 +1,13 @@
+ const timerDisplay = document.getElementById("timer");
+ const startBtn = document.getElementById("startBtn");
+
+ // the time we are starting with - 25 mn 
+
+ let timeleft = 25 * 60;
+
+
+
+ // function to start the timer 
+ startBtn.addEventListener("click", ()=>{
+
+ })
