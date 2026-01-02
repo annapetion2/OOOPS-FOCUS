ee6aaa9ad8bff814f1c662809152ff8a376ae544
Anna Petion <annapetion2@gmail.com>
2026-01-01 20:58:02 -0500

    what pulled you away is done

--- OopsFocus.js
+++ OopsFocus.js
@@ -1,10 +1,11 @@
  // grabbing the element 
- 
  const timerDisplay = document.getElementById("timer");
  const startBtn = document.getElementById("startBtn"); // start button 
  const pauseBtn = document.getElementById("pauseBtn"); // pause button 
  const resetBtn = document.getElementById("resetBtn"); // reset button 
+ const distractionDisplay = document.querySelector(".distraction-list");
 
+ const distractionItem = document.querySelectorAll(".distraction-item");
 
 
  // the time we are starting with - 25 mn 
@@ -12,6 +13,7 @@
  let timeLeft = 25 * 60; // mn in sec 
  let timerInterval = 0;
  let isRunning = false;
+ let counterItem = 0; 
 
 
 // timer display 
@@ -66,3 +68,33 @@ resetBtn.addEventListener("click", () =>{
   timeLeft = 25 * 60;
   updateDisplay();
 })
+
+
+
+// function to make sure distraction items increments 
+function updateTotal() {
+  let total = 0;
+  // every count-val on the page and add them 
+  document.querySelectorAll('.count-val').forEach(el => {
+    total += parseInt(el.innerText);
+  });
+  // distraction-amount h6 
+  document.querySelector('.distraction-amount').innerText = "Distraction(s): " + total;
+}
+
+function increment(btn) {
+  // Find the specific span inside the clicked button
+  const countSpan = btn.querySelector('.count-val');
+  
+  // Get current text convert to number & add 1
+  let currentVal = parseInt(countSpan.innerText);
+  countSpan.innerText = currentVal + 1;
+
+  updateTotal();
+}
+
+distractionItem.forEach(item => {
+  item.addEventListener("click", () => {
+    increment(item); 
+  });
+});
